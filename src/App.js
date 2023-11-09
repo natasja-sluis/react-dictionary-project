39543b8d0968805e95b12357e20b9a9bfb492cf9
Twesha33 <106900207+Twesha33@users.noreply.github.com>
2023-11-09 08:04:32 +0100

    Added github link

--- src/App.js
+++ src/App.js
@@ -6,7 +6,25 @@ function App() {
     <div className="container">
       <h1 className="Title">Dictionary App</h1>
       <Dictionary />
-      <footer className="Footer">coded by Natasja Sluis</footer>
+      <footer className="Footer">
+        coded by{" "}
+        <a
+          href="https://natasjasluis.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+          class="link"
+        >
+          Natasja Sluis
+        </a>{" "}
+        <a
+          href="https://github.com/Twesha33/react-dictionary-project"
+          target="_blank"
+          rel="noreferrer"
+          class="link"
+        >
+          <i class="fa-brands fa-github"></i>
+        </a>
+      </footer>
     </div>
   );
 }
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -28,7 +28,7 @@ export default function Dictionary() {
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
-        <input type="search" onChange={handleKeywordChange} />
+        <input class="search" type="search" onChange={handleKeywordChange} />
       </form>
       <Results results={results} />
     </div>
